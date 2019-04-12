2bf9ca170b9b6d9028f2d01f1905b3d52949cb40
Michael Lass <michael.lass@uni-paderborn.de>
2019-04-12 13:26:59 +0200

    Sync script to push a snapshot to Sciebo

---
+++ sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TARGET=~/Sciebo/rengaraj19_tbd_snapshot/
+
+[ -d "$TARGET" ] || exit 1
+
+HEAD=$(cat "$TARGET"/head)
+[ "x$HEAD" == "x" ] && exit 1
+echo "Snapshot is at $HEAD"
+
+DIFF=$(git --work-tree="$TARGET" diff $HEAD)
+
+if [ "x$DIFF" != "x" ]; then
+  echo "ERROR: There have been changes done to the snapshot:"
+  echo "$DIFF"
+  exit 1
+fi
+
+git --work-tree="$TARGET" checkout HEAD -- .
+git rev-parse HEAD > "$TARGET"/head
+
+echo "Files copied."
+echo "Snapshot is now at $(cat "$TARGET"/head)"
+
+STALE=$(git --work-tree="$TARGET" clean -xdn -e head -- .)
+if [ "x$STALE" != "x" ]; then
+  echo "WARNING: Stale files found in snapshot:"
+  echo "$STALE"
+  echo "Run the following command to clean up:"
+  echo "git --work-tree="$TARGET" clean -xdf -e head -- ."
+fi
